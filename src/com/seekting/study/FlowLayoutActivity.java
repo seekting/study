9c36ed1260fa0187d3046f0a8fd878e816c27225
seekting <543090717@qq.com>
2013-10-31 12:21:39 +0800

    [add]FlowLayout支持

---
+++ src/com/seekting/study/FlowLayoutActivity.java
@@ -0,0 +1,19 @@
+
+package com.seekting.study;
+
+import android.os.Bundle;
+
+public class FlowLayoutActivity extends BaseActivity {
+
+    public FlowLayoutActivity() {
+        name = "flowLayout流式布局";
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flowlayout_activity);
+
+    }
+}
