e7e28eddbf3203993fea196a5abd209aa68931e5
seekting <543090717@qq.com>
2014-02-18 12:36:09 +0800

    [add] actionbar

--- src/com/seekting/study/SuperListViewActivity.java
+++ src/com/seekting/study/SuperListViewActivity.java
@@ -6,11 +6,17 @@ import java.util.LinkedHashMap;
 import java.util.List;
 
 import android.app.ActionBar;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.seekting.study.view.SectionListView;
 import com.seekting.study.view.SectionListView.City;
@@ -28,13 +34,13 @@ public class SuperListViewActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+
+        setContentView(R.layout.select_city_layout);
         ActionBar actionBar = getActionBar();
         actionBar.setIcon(getResources().getDrawable(R.drawable.action_bar_icon));
         actionBar.setTitle("Ñ¡Ôñ³ÇÊÐ");
+        actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.kui_address_bar_bg));
-
-        setContentView(R.layout.select_city_layout);
-
         mSearchCityEditext = (EditText) findViewById(R.id.search_city_edt);
         mSearchCityEditext.addTextChangedListener(new TextWatcher() {
 
@@ -57,6 +63,17 @@ public class SuperListViewActivity extends BaseActivity {
         });
         mSectionListView = (SectionListView) findViewById(R.id.super_list_view);
         change();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 
